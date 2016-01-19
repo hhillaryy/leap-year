ad2ccdd9513d721c9170cf128663036a291344a5
Hillary Hintzen <hillary07@gmail.com>
2016-01-19 15:02:06 -0800

    fix errant code from weird merge to make site work again

--- js/scripts.js
+++ js/scripts.js
@@ -1,46 +1,39 @@
 var leapYear = function(year) {
-<<<<<<< HEAD
   if (isNaN(year) || (year < 0)) {
     return null;
   } else {
-    return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
+    return ((year % 4 === 0) && (year % 100 !== 0)) || (year % 400 === 0);
   }
-=======
-  return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
->>>>>>> 613d1151bac0b53a28d41dd31482bf7383fb37d8
-};
+}
 
 $(document).ready(function() {
   $("form#leap-year").submit(function(event) {
     var year = parseInt($("input#year").val());
     var result = leapYear(year);
 
-<<<<<<< HEAD
     if (result === null) {
       alert("Please enter a number")
     } else {
       $(".year").text(year);
 
       if (!result) {
-        $(".not").text("not");
-      } else {
-        $(".not").text("");
+          $(".not").text("not");
+        } else {
+          $(".not").text("");
+        }
+
+        $("#result").show();
       }
 
-      $("#result").show();
-    }
-
-=======
-    $(".year").text(year);
-
-    if (!result) {                 // same as writing if (result === false)
-      $(".not").text("not");
-    } else {
-      $(".not").text("");
-    }
-
-    $("#result").show();
->>>>>>> 613d1151bac0b53a28d41dd31482bf7383fb37d8
+    // $(".year").text(year);
+    //
+    // if (!result) {
+    //   $(".not").text("not");
+    // } else {
+    //   $(".not").text("");
+    // }
+    //
+    // $("#result").show();
     event.preventDefault();
   });
 });
